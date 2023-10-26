42757ebfa0f3b5eccd0b3ffbfe8038d58e68f443
yogita-assi <ypurane@assimitatetechonlogies.com>
2023-10-26 17:44:28 +0530

    folder structure

---
+++ AriesAgro/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView ><>
+      <Text>ARIS AGRO</Text>
+      </>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
