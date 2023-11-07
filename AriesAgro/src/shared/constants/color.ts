e335e3f399821f51d09dd4c6353c0c33b64891a0
payal kharat <pkharat@assimilatetechnologies.com>
2023-11-07 11:49:53 +0530

    add color

--- AriesAgro/src/shared/constants/color.ts
+++ AriesAgro/src/shared/constants/color.ts
@@ -16,7 +16,7 @@ export const WHITE_BACKGROUND = '#EEEFF4';
 export const GRAY_SHADE = "#717171";
 export const RAISIAN_BLACK = "#231F20";
 export const NAVYBULE = '#303342';
-export const LIGHTGREY = "#D2D2D2";
+export const LIGHTGREY = "#555656";
 export const EXTRA_LIGHTGREY = "#D3D3D3";
 export const DISABLED = "#C4C4C4";
 export const LIGHTBLUE = "#1D2149";
@@ -97,4 +97,5 @@ export const ARCHIVO_EXTRALIGHT = "Mulish-ExtraLight";
 export const ARCHIVO_MEDIUN = "Mulish-Medium";
 export const ARCHIVO_REGULAR = "Mulish-Regular";
 export const ARCHIVO_SEMIBOLD = "Mulish-SemiBold";
+export const GREY_LIGHT="#D2D2D2"
 
